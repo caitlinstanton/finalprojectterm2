89bf19459fe1f4dd9aea803ea360d307b6b9886d
Caitlin Stanton <livelaughlovelol4@yahoo.com>
2015-06-11 15:35:14 -0400

    Try this for Driver

---
+++ Switch.java
@@ -0,0 +1,24 @@
+public class Switch {
+
+    public static void wee(int ex){
+    switch (ex) {
+    case 0:
+	System.out.println("This is zero!");
+	break;
+    case 34: 
+	System.out.println("34 YO!");
+	break;
+    default:
+	System.out.println("noooooooooooooooooo");
+	break;
+	    
+    }
+    }
+    public static void main(String[]args){
+	int t = 2456;
+
+	wee(t);
+    }
+    
+
+}
