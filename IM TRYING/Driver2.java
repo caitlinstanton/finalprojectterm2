c4bcccc02e728c5e338a39f4e7f183991ccf0ed7
Your Name <livelaughlovelol4@yahoo.com>
2015-06-12 08:38:12 -0400

    C

--- IM TRYING/Driver2.java
+++ IM TRYING/Driver2.java
@@ -1,30 +1,46 @@
+import java.util.*;
+import java.io.*;
+
 public class Driver2 {
 
     public static void main(String[] args) {	
+	Scanner in = new Scanner(System.in);
 	Gradebook stuy = new Gradebook();
+	
+
+
 	stuy.addTeacher("Zamansky");
 	stuy.addTeacher("EMILY IS WEIRD");
 	stuy.addTeacher("Caitliiin");
-
-	System.out.println(stuy);
-
-	System.out.println();
-
 	stuy.getTeacher("Zamansky").add("C", 35);
-	System.out.println(stuy.getTeacher("Zamansky").getCourse());
-
-	System.out.println();
-
 	stuy.getTeacher("Zamansky").findCourse("C").addStudent("A");
 	stuy.getTeacher("Zamansky").findCourse("C").addStudent("B");
 	stuy.getTeacher("Zamansky").findCourse("C").addStudent("C"); 
-	System.out.println(stuy.getTeacher("Zamansky").findCourse("C").getStudents());   
-
-	System.out.println();
-
 	stuy.getTeacher("Zamansky").findCourse("C").addGrades("A", "blah", 109);
+	System.out.println(stuy.getTeacher("Zamansky").getCourse());
 
-	System.out.println(stuy.getTeacher("Zamansky").findCourse("C").getStudents());   
+	System.out.println("Hello and welcome to StuyGrades, a gradebook designed by Caitlin Stanton and Michael Mattson!");
+	System.out.println("Would you like to add a teacher or find a teacher? Type 'add' for the former or 'find'.");
+	String action = in.nextLine();
+	switch (action) {
+	case "add":
+	    System.out.println("You are now going to add a teacher. Please type in the last name of the teacher.");
+	    String add = in.nextLine();
+	    stuy.addTeacher(add);
+	    System.out.println(stuy);
+	    break;
+	case "find":
+	    System.out.println("You are now going to find a teacher. Please type in the last name of the teacher.");
+	    String find = in.nextLine();
+
+	    //WHYYYYYYYYYYYYYYYYYYYYYYYYYYYY
+	    System.out.println(stuy.getTeacher("Zamansky").getCourse());
+
+	    break;
+	default:
+	    System.out.println("Please try again");
+	    break;
+	}
 
     }
 
